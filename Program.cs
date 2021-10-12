06dba9eb707a40a7b6ceadc67996e47c470b01e0
Sokhour LACH <lachsokhour168@gmail.com>
2021-10-12 20:48:29 +0700

    Create Virtual Class

--- Program.cs
+++ Program.cs
@@ -4,9 +4,37 @@ namespace ooad_csharp_oop
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        public static void Main(string[] args) {
+            // ឧទាហរណ៍ទី១
+            double distance, hour, fuel = 0.0;
+            Console.Write("Enter the Distance: ");
+            distance = Double.Parse(Console.ReadLine());
+            Console.Write("Enter the Hours: ");
+            hour = Double.Parse(Console.ReadLine());
+            Console.Write("Enter the Fuel: ");
+            fuel = Double.Parse(Console.ReadLine());
+            
+            Car objCar = new Car(distance, hour, fuel);
+            Vehicle objVeh = objCar;
+            objCar.Average();
+            objVeh.Average();
+            objCar.Speed();
+            objVeh.Speed();
+            Console.ReadKey(); 
+
+            // ឧទាហរណ៍ទី២
+            double length, width, radius = 0.0;
+            Console.Write("Enter the Length: ");
+            length = Double.Parse(Console.ReadLine());
+            Console.Write("Enter the Width: ");
+            width = Double.Parse(Console.ReadLine());
+            Rectangle objRectangle = new Rectangle(length, width);
+            objRectangle.Area();
+            Console.Write("Enter the Radius: ");
+            radius = Double.Parse(Console.ReadLine());
+            Circle objCircle = new Circle(radius);
+            objCircle.Area();
+            Console.ReadKey(); 
         }
     }
 }
