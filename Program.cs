1ba86546f54e26da490daa424dabbf6df777ad83
Sokhour LACH <lachsokhour168@gmail.com>
2021-10-12 20:43:32 +0700

    init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ooad_csharp_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
